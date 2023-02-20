b24087e2ccb38846f0d11a96e930c7ccc1879bab
= <=>
2023-02-20 12:44:11 -0500

    "hechos los edits"

--- src/main/java/com/sugggarCoffe/sub/controller/ProveedorController.java
+++ src/main/java/com/sugggarCoffe/sub/controller/ProveedorController.java
@@ -21,8 +21,6 @@ public class ProveedorController {
 	@Autowired
 	private ProveedorService proveedorService;
 
-	
-	
 	@GetMapping("/listProveedor")
 	public String getAllProveedor(Model model) {
 		List<Proveedor> listProveedor  =proveedorService.listarProveedor();
@@ -48,20 +46,6 @@ public class ProveedorController {
 		proveedorService.deleteProveedor(proveedor);
 		return "redirect:/listProveedor";
 	}
-	
-	 @GetMapping("/proveedor/{id}/edit")
-	  public String edit(@PathVariable Long id, Model model) throws NotFoundException {
-	      model.addAttribute("proveedor", proveedorService.getProveedor(id));
-	      return "inventario/listProveedor";
-	  }
-	
-	  @PostMapping("/proveedor/{id}/edit")
-	  public String edit(Proveedor proveedor) {
-	  	proveedorService.createProveedor(proveedor);
-	      return "redirect:/listProveedor";
-	  }
-
-
 
 }
 
@@ -73,53 +57,3 @@ public class ProveedorController {
 
 
 
-
-//@PostMapping("/listProveedor")
-//public String create(Proveedor proveedor) {
-//	proveedorService.createProveedor(proveedor);
-//  return "redirect: inventario/proveedorDetalle";
-//}
-//
-//
-//@GetMapping("/listProveedor")
-//  public String list(Model model) {
-//      model.addAttribute("proveedor", proveedorService.listarProveedor());
-//      return "inventario/proveedorDetalle";
-//  }
-//
-//  @GetMapping("/proveedor/create")
-//  public String create(Model model) {
-//      model.addAttribute("product", new Proveedor());
-//      return "inventario/proveedorDetalle";
-//  }
-//
-//  
-//
-//  @GetMapping("/proveedor/{id}/edit")
-//  public String edit(@PathVariable Long id, Model model) throws NotFoundException {
-//      model.addAttribute("proveedor", proveedorService.getProveedor(id));
-//      return "inventario/proveedorDetalle";
-//  }
-//
-//  @PostMapping("/proveedor/{id}/edit")
-//  public String edit(Proveedor proveedor) {
-//  	proveedorService.createProveedor(proveedor);
-//      return "redirect:/inventario/proveedorDetalle*";
-//  }
-
-//@PostMapping("/proveedor/{id}/edit")
-//public String edit(Proveedor proveedor) {
-//	proveedorService.createProveedor(proveedor);
-//  return "redirect:/inventario/proveedorDetalle*";
-//}
-  
- 
-  
-//  @GetMapping("/proveedor/{id}/edit")
-//  public String edit(@PathVariable Long id, Model model) throws NotFoundException {
-//      model.addAttribute("proveedor", proveedorService.getProveedor(id));
-//      return "inventario/proveedorDetalle";
-//  }
-//
-
-//
--- src/main/resources/static/js/toEditAll.js
+++ src/main/resources/static/js/toEditAll.js
@@ -1,19 +1,127 @@
-function getValorAtributoP(atributo) {
-	
-  let inputValor = document.querySelectorAll('input');
-  let valores = [];
-  
-  for (let i = 0; i < inputValor.length; i++) {   
-    const valor = inputValor[i].getAttribute(atributo);  
-    valores.push(valor);
-  }
+// Obtener referencias a la tabla y los labels
 
-  let pValor = document.querySelectorAll('p');
-	for (let i = 0; i < pValor.length; i++) {
-    	 pValor[i].setAttribute('th:text',"*{"+valores[i]+"}");	
-  	}
+
+function editarProveedor(){
+const tabla = document.getElementById('tablaProveedor');
+const botonesVerDetalles = document.querySelectorAll('.btnEditar');
+botonesVerDetalles.forEach(boton => {
+  boton.addEventListener('click', function() {
+   															 					// Obtener la fila correspondiente al botón
+    const fila = boton.parentNode.parentNode;
+	alert("Por favor verifique todos los datos antes de guardar");
+    																			// Obtener los datos de la fila
+    const idProveedor = fila.querySelector('td:nth-child(1)').textContent;
+    const razonSocial = fila.querySelector('td:nth-child(2)').textContent;
+    const nombreContacto = fila.querySelector('td:nth-child(3)').textContent;
+    const correoProveedor = fila.querySelector('td:nth-child(5)').textContent;
+    const numeroProveedor = fila.querySelector('td:nth-child(6)').textContent;
+    const direccionProveedor = fila.querySelector('td:nth-child(7)').textContent;
+    
+    																			// Mostrar los datos en los labels correspondientes
+    document.getElementById('idProveedor').value=idProveedor;
+	document.getElementById('razonSocial').value=razonSocial ;
+	document.getElementById('nombreContacto').value=nombreContacto ;
+	document.getElementById('correoProveedor').value=correoProveedor;
+	document.getElementById('numeroProveedorP').value=numeroProveedor ;
+	document.getElementById('direccionProveedor').value=direccionProveedor ;
+    
+  });
+});
 }
 
+function editarInsumo(){
+  const tabla = document.getElementById('tablaInsumo');
+  const botonesVerDetalles = document.querySelectorAll('.btnEditar');
+  botonesVerDetalles.forEach(boton => {
+    boton.addEventListener('click', function() {
+                                              // Obtener la fila correspondiente al botón
+      const fila = boton.parentNode.parentNode;
+    alert("Por favor verifique todos los datos antes de guardar");
+                                            // Obtener los datos de la fila
+      const idInsumo = fila.querySelector('td:nth-child(1)').textContent;
+      const nombreInsumo = fila.querySelector('td:nth-child(2)').textContent;
+      const precioInsumo = fila.querySelector('td:nth-child(3)').textContent;
+      
+                                            // Mostrar los datos en los labels correspondientes
+      document.getElementById('idInsumo').value=idInsumo;
+    document.getElementById('nombreInsumo').value=nombreInsumo;
+    document.getElementById('precioInsumo').value=precioInsumo;   
+    });
+  }); 
+  }
 
+  function editarProducto(){
+    const tabla = document.getElementById('tablaProducto');
+    const botonesVerDetalles = document.querySelectorAll('.btnEditar');
+    botonesVerDetalles.forEach(boton => {
+      boton.addEventListener('click', function() {
+                                                // Obtener la fila correspondiente al botón
+        const fila = boton.parentNode.parentNode;
+      alert("Por favor verifique todos los datos antes de guardar");
+                                              // Obtener los datos de la fila
+        const idProducto = fila.querySelector('td:nth-child(1)').textContent;
+        const nombreProducto = fila.querySelector('td:nth-child(2)').textContent;
+        const precioProducto = fila.querySelector('td:nth-child(3)').textContent;
+        const descripcionP = fila.querySelector('td:nth-child(7)').textContent;
+        
+                                              // Mostrar los datos en los labels correspondientes
+      document.getElementById('idProducto').value=idProducto;
+      document.getElementById('nombreProducto').value=nombreProducto;
+      document.getElementById('precioProducto').value=precioProducto;
+      document.getElementById('descripcionP').value=descripcionP;
+        
+      });
+    });
+    }
 
+    function editarUsuario(){
+      const tabla = document.getElementById('tablaUsuario');
+      const botonesVerDetalles = document.querySelectorAll('.btnEditar');
+      botonesVerDetalles.forEach(boton => {
+        boton.addEventListener('click', function() {
+                                                  // Obtener la fila correspondiente al botón
+          const fila = boton.parentNode.parentNode;
+        alert("Por favor verifique todos los datos antes de guardar");
+                                                // Obtener los datos de la fila
+          const idUsuario = fila.querySelector('td:nth-child(1)').textContent;
+          const nombreUsuario = fila.querySelector('td:nth-child(2)').textContent;
+          const correoUsuario = fila.querySelector('td:nth-child(5)').textContent;
+          const numeroUsuario = fila.querySelector('td:nth-child(6)').textContent;
+          const direccionUsuario = fila.querySelector('td:nth-child(7)').textContent;
+          const descripcionUsuario = fila.querySelector('td:nth-child(8)').textContent;
+          
+                                                // Mostrar los datos en los labels correspondientes
+          document.getElementById('idUsuario').value=idUsuario;
+        document.getElementById('nombreUsuario').value=nombreUsuario;
+        document.getElementById('correoUsuario').value=correoUsuario;
+        document.getElementById('numeroUsuario').value=numeroUsuario;
+        document.getElementById('direccionUsuario').value=direccionUsuario;
+        document.getElementById('descripcionUsuario').value=descripcionUsuario;
+          
+        });
+      });
+      }
 
+      function editarCliente(){
+        const tabla = document.getElementById('tablaCliente');
+        const botonesVerDetalles = document.querySelectorAll('.btnEditar');
+        botonesVerDetalles.forEach(boton => {
+          boton.addEventListener('click', function() {
+                                                    // Obtener la fila correspondiente al botón
+            const fila = boton.parentNode.parentNode;
+          alert("Por favor verifique todos los datos antes de guardar");
+                                                  // Obtener los datos de la fila
+            const idCliente = fila.querySelector('td:nth-child(1)').textContent;
+            const nombreCliente = fila.querySelector('td:nth-child(2)').textContent;
+            const telefonoCliente = fila.querySelector('td:nth-child(3)').textContent;
+            const direccionCliente = fila.querySelector('td:nth-child(4)').textContent;
+            
+                                                  // Mostrar los datos en los labels correspondientes
+            document.getElementsByName('idCliente').value=idCliente;
+          document.getElementByName('nombreCliente').value=nombreCliente;
+          document.getElementByName('telefonoCliente').value=telefonoCliente;
+          document.getElementByName('direccionCliente').value=direccionCliente;
+            
+          });
+        });
+        }
